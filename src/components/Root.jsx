adc4c69e2debb31e0ff924dab677144902e78450
Виталик Матях <1seestars@MacBook-Air-Vitalik.local>
2020-08-27 22:18:59 +0300

    fix

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import MainComponent from './MainComponent'
-import ScoreItem from './ScoreItem'
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as HashRouter, Route } from "react-router-dom";
+import MainComponent from "./MainComponent";
+import ScoreItem from "./ScoreItem";
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <Router>
+    <HashRouter basename="/">
       <Route exact path="/" component={MainComponent} />
-      <Route path='/:id' component={ScoreItem} />
-    </Router>
+      <Route path="/:id" component={ScoreItem} />
+    </HashRouter>
   </Provider>
-)
+);
 
-export default Root
+export default Root;
