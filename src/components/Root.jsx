9c1539702a4aa84136a85ebee817566b4d8c5f8e
Виталик Матях <1seestars@MacBook-Air-Vitalik.local>
2020-08-27 20:31:17 +0300

    fix

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,23 +1,16 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import MainComponent from "./MainComponent";
-import ScoreItem from "./ScoreItem";
+import React from 'react'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import MainComponent from './MainComponent'
+import ScoreItem from './ScoreItem'
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router>
-      <Route
-        exact
-        path="/https://1seestars.github.io/task-timer-app-deploy/"
-        component={MainComponent}
-      />
-      <Route
-        path="/https://1seestars.github.io/task-timer-app-deploy/:id"
-        component={ScoreItem}
-      />
+      <Route exact path="/" component={MainComponent} />
+      <Route path='/:id' component={ScoreItem} />
     </Router>
   </Provider>
-);
+)
 
-export default Root;
+export default Root
