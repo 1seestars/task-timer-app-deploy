a79d67c95af99cda3f2db0d6ed0a8817fe945abd
Виталик Матях <1seestars@MacBook-Air-Vitalik.local>
2020-08-27 23:38:11 +0300

    fix

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { createBrowserHistory } from "history";
 import MainComponent from "./MainComponent";
 import ScoreItem from "./ScoreItem";
 
-const history = createBrowserHistory();
-
 const Root = ({ store }) => (
   <Provider store={store}>
-    <Router history={history}>
+    <Router>
       <Route exact path="/" component={MainComponent} />
       <Route path="/:id" component={ScoreItem} />
     </Router>
